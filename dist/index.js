d89ee10eedd09c180ed19b9071aa3d915f2fe395
Amit Sharma <amit_sharma@rapid7.com>
2016-10-17 21:21:02 +0100

    add dist

---
+++ dist/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.SampleProject5 = exports.SampleProject3 = exports.SampleProject2 = exports.SampleProject1 = undefined;
+
+var _sampleProject1Bundle = require('./sampleProject1.bundle.js');
+
+var _sampleProject1Bundle2 = _interopRequireDefault(_sampleProject1Bundle);
+
+var _sampleProject2Bundle = require('./sampleProject2.bundle.js');
+
+var _sampleProject2Bundle2 = _interopRequireDefault(_sampleProject2Bundle);
+
+var _sampleProject3Bundle = require('./sampleProject3.bundle.js');
+
+var _sampleProject3Bundle2 = _interopRequireDefault(_sampleProject3Bundle);
+
+var _sampleProject5Bundle = require('./sampleProject5.bundle.js');
+
+var _sampleProject5Bundle2 = _interopRequireDefault(_sampleProject5Bundle);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.SampleProject1 = _sampleProject1Bundle2.default;
+exports.SampleProject2 = _sampleProject2Bundle2.default;
+exports.SampleProject3 = _sampleProject3Bundle2.default;
+exports.SampleProject5 = _sampleProject5Bundle2.default;
